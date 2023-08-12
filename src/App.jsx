7bb4d102c8a8ea5e0f0e535a85f511995f120364
Terry Nguyen <terryknguyen@gmail.com>
2023-08-12 19:46:03 -0400

    add 404.html

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
     return (
         <BrowserRouter basename={ process.env.PUBLIC_URL }>
             <Routes>
-                <Route path="" element={user ? <Home /> : <Register />} />
+                <Route path="/" element={user ? <Home /> : <Register />} />
                 <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
                 <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
                 <Route path="/profile/:username" element={<Profile />} />
