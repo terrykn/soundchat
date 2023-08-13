cd676f142bdcfbc59d2085afca073195489bd01c
Terry Nguyen <terryknguyen@gmail.com>
2023-08-12 20:23:55 -0400

    test

--- src/index.js
+++ src/index.js
@@ -31,6 +31,7 @@ ReactDOM.render(
     <React.StrictMode>
       <AuthContextProvider>
         <div style={{ backgroundColor: theme.palette.background.default }}>
+          <h1>hi</h1>
           <App />
         </div>
       </AuthContextProvider>
