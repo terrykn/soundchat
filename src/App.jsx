f3d6de66eb6144eafb4c4b2e2a07db30fd10579f
Terry Nguyen <terryknguyen@gmail.com>
2023-08-12 20:21:35 -0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,17 @@ import { AuthContext } from "./context/AuthContext";
 const App = () => {
     const { user } = useContext(AuthContext);
     return (
-        <HashRouter>
-            <Routes>
-                <Route path="/" element={user ? <Home /> : <Register />} />
-                <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
-                <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
-                <Route path="/profile/:username" element={<Profile />} />
-            </Routes>
-        </HashRouter>
+        <div>
+            <h1>HELLO</h1>
+            <HashRouter>
+                <Routes>
+                    <Route path="/" element={user ? <Home /> : <Register />} />
+                    <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+                    <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
+                    <Route path="/profile/:username" element={<Profile />} />
+                </Routes>
+            </HashRouter>
+        </div>
     );
 }
 
